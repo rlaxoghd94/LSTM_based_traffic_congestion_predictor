9e761bf2e0e9e818d7722ceb4d30b1d88de310e2
Nic Taehong Kim <ktae.nic@gmail.com>
2018-12-03 02:38:46 +0900

    major change

---
+++ crawling_tool.py
@@ -0,0 +1,135 @@
+import requests
+import lxml.html as lh
+import pandas as pd
+
+
+def parseURL(url):
+    # create a handle, page, to handle the contents of the website
+    page = requests.get(url)
+
+    # store the contents of the website under doc
+    doc = lh.fromstring(page.content)
+
+    # parse <tr>
+    tr_elements = doc.xpath("/html/body/div[@id='wrapper']/div[@id='container']//tr")
+
+    # create empty list
+    col = []
+    i = 0
+
+    # for each row, store each first element (header) and an empty list
+    for tr_data in tr_elements:
+        td_elements = doc.xpath("/html/body/div[@id='wrapper']/div[@id='container']//tr[{}]//td".format(i))
+        j = 0
+
+        '''
+        rowData[]
+        1. 구간시점
+        2. 구간종점
+        3. 속도
+        4. 거리
+        '''
+        rowData = []
+
+        for td_data in td_elements:
+            # print('\t[{}] {} - {}'.format(j, td_data.text_content(), type(td_data)))
+            if i == 2:
+                if j == 1:
+                    # section
+                    rowData.append(td_data.text_content())
+                elif j == 3:
+                    # terminal section
+                    rowData.append(td_data.text_content())
+                elif j == 4:
+                    # speed
+                    rowData.append(td_data.text_content())
+                elif j == 5:
+                    # duration
+                    rowData.append(td_data.text_content())
+            else:
+                if j == 0:
+                    # section
+                    rowData.append(td_data.text_content())
+                elif j == 2:
+                    # terminal section
+                    rowData.append(td_data.text_content())
+                elif j == 3:
+                    # speed
+                    rowData.append(td_data.text_content())
+                elif j == 4:
+                    # duration
+                    rowData.append(td_data.text_content())
+            j += 1
+        col.append(rowData)
+        i += 1
+
+    # print col for safety
+    del col[0]  # MUST del first and last element
+    del col[-1]  # it contains empty elements
+
+    # printCol(col)
+    col = dataInpection(col)
+    # printCol(col)
+    return refineData(col)
+
+
+def printCol(col):
+    k = 0
+    for row in col:
+        print('\t[{}] row'.format(k))
+        for elem in row:
+            print('\t\t[+] - {}'.format(elem))
+        k += 1
+
+    '''
+    rowData[]
+    0. 구간시점
+    1. 구간종점
+    2. 속도
+    3. 거리
+    '''
+
+
+def dataInpection(col):
+    colLen = len(col)
+    nextIdx = 1
+    curIdx = 0
+
+    while nextIdx != (colLen - 1):
+        cur = col[curIdx]
+        next = col[nextIdx]
+        if '휴게소' in cur[1]:
+            tempP = cur[1]
+            cur[1] = next[1]
+            curSpeed = cur[2]
+            nextSpeed = next[2]
+            cur[2] = str(int((int(curSpeed[:-4]) + int(nextSpeed[:-4])) / 2)) + 'km/h'
+            curTime = cur[3]
+            nextTime = next[3]
+            cur[3] = str((int(curTime[:-1]) + int(nextTime[:-1]))) + '분'
+            print('\t\t\t[removed]-{}'.format(tempP))
+            col.remove(col[nextIdx])
+            colLen -= 1
+        curIdx += 1
+        nextIdx += 1
+    return col
+
+
+def refineData(col):
+    data = {}
+    sectionData = []
+    sectionName = []
+
+    for i in range(0, len(col)):
+        speed = col[i][2]
+        sectionData.append(speed[:-4])
+        start = col[i][0]
+        end = col[i][1]
+        strEnd = start + ' -> ' + end
+        sectionName.append(strEnd)
+
+    data['data'] = sectionData
+    data['name'] = sectionName
+    return data
+
+# parseURL(url_up)
--- route.py
+++ route.py
@@ -1,20 +1,166 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
+import pandas as pd
 import os
+import time
+import numpy as np
+import json
+from crawling_tool import parseURL
+
+# 서울외곽순환도로
+up_1000 = 'http://www.roadplus.co.kr/trafficState.do?roadIdS=H1002&roadIdE=H1001&roadName=%EC%84%9C%EC%9A%B8%EC%99%B8%EA%B3%BD%EC%88%9C%ED%99%98%EA%B3%A0%EC%86%8D%EB%8F%84%EB%A1%9C&mCode=A050010&op=highwayItem&highway=&bound=E&state=small&onPage=&noTab='
+down_1000 = 'http://www.roadplus.co.kr/trafficState.do?roadIdS=H1002&roadIdE=H1001&roadName=%EC%84%9C%EC%9A%B8%EC%99%B8%EA%B3%BD%EC%88%9C%ED%99%98%EA%B3%A0%EC%86%8D%EB%8F%84%EB%A1%9C&mCode=A050010&op=highwayItem&highway=&bound=S&state=small&onPage=&noTab='
+
+# 경인고속도로
+up_1200 = 'http://www.roadplus.co.kr/trafficState.do?roadIdS=H1202&roadIdE=H1201&roadName=%EA%B2%BD%EC%9D%B8%EA%B3%A0%EC%86%8D%EB%8F%84%EB%A1%9C&mCode=A050010&op=highwayItem&highway=&bound=E&state=small&onPage=&noTab='
+down_1200 = 'http://www.roadplus.co.kr/trafficState.do?roadIdS=H1202&roadIdE=H1201&roadName=%EA%B2%BD%EC%9D%B8%EA%B3%A0%EC%86%8D%EB%8F%84%EB%A1%9C&mCode=A050010&op=highwayItem&highway=&bound=S&state=small&onPage=&noTab='
+
+# 서해안고속도로
+up_0150 = 'http://www.roadplus.co.kr/trafficState.do?roadIdS=H0152&roadIdE=H0151&roadName=%EC%84%9C%ED%95%B4%EC%95%88%EA%B3%A0%EC%86%8D%EB%8F%84%EB%A1%9C&mCode=A050010&op=highwayItem&highway=&bound=E&state=small&onPage=&noTab='
+down_0150 = 'http://www.roadplus.co.kr/trafficState.do?roadIdS=H0152&roadIdE=H0151&roadName=%EC%84%9C%ED%95%B4%EC%95%88%EA%B3%A0%EC%86%8D%EB%8F%84%EB%A1%9C&mCode=A050010&op=highwayItem&highway=&bound=S&state=small&onPage=&noTab='
+
+# 서울양양고속도로
+up_0600 = 'http://www.roadplus.co.kr/trafficState.do?roadIdS=H0604&roadIdE=H0603&roadName=%EC%84%9C%EC%9A%B8%EC%96%91%EC%96%91%EA%B3%A0%EC%86%8D%EB%8F%84%EB%A1%9C&mCode=A050010&op=highwayItem&highway=&bound=E&state=small&onPage=&noTab='
+down_0600 = 'http://www.roadplus.co.kr/trafficState.do?roadIdS=H0604&roadIdE=H0603&roadName=%EC%84%9C%EC%9A%B8%EC%96%91%EC%96%91%EA%B3%A0%EC%86%8D%EB%8F%84%EB%A1%9C&mCode=A050010&op=highwayItem&highway=&bound=S&state=small&onPage=&noTab='
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static')
 CORS(app)
+
+tempData = {}
+
 
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+
 @app.route('/redefined')
 def redefined():
-    return render_template('refined_index.html')
+    return render_template('refined_index.html', data=json.dumps(tempData))
+
+
+@app.route("/tempData")
+def geo_code():
+    return json.dumps(tempData)
+
+
+@app.route('/getChartData/<roadName>')
+def getData(roadName):
+    global tempData
+    road = roadName[:-2]
+    url = ""
+
+    if '1000_E' in roadName:
+        url = up_1000
+        print()
+    elif '1000_S' in roadName:
+        url = down_1000
+        print()
+    elif '1200_E' in roadName:
+        url = up_1200
+        print()
+    elif '1200_S' in roadName:
+        url = down_1200
+        print()
+    elif '0150_E' in roadName:
+        url = up_0150
+        print()
+    elif '0150_S' in roadName:
+        url = down_0150
+        print()
+    elif '0600_E' in roadName:
+        url = up_0600
+        print()
+    elif '0600_S' in roadName:
+        url = up_0600
+        print()
+    else:
+        print('\t[+] ERROR in roadName request - {}'.format(roadName))
+        return
+
+    parsedList = parseURL(url)
+    data = formatData(roadName[-1], parsedList, road)
+    temp_csv(data, road)
+    print(tempData)
+    return json.dumps(tempData)
+
+
+def temp_csv(data, road):
+    global tempData
+    csv_E = pd.read_csv('1000_E.csv')
+    currentHr = int(time.strftime("%H")) + 12
+    pastFutureData = csv_E.values[0:, currentHr + 1:currentHr + 4]
+    tempData['data'] = pastFutureData.tolist()
+    combineData(data, road)
+
+
+def init():
+    global tempData
+    parsedE_1000 = parseURL(up_1000)
+    parsedS_1000 = parseURL(down_1000)
+    dataE_1000 = formatData("E", parsedE_1000, '1000')
+    dataS_1000 = formatData("S", parsedS_1000, '1000')
+    # parsedE_1200 = parseURL(up_1200)
+    # parsedS_1200 = parseURL(down_1200)
+    # dataE_1200 = formatData("E", parsedE_1200, '1200')
+    # dataE_1200 = formatData("S", parsedS_1200, '1200')
+    # parsedE_0150 = parseURL(up_0150)
+    # parsedS_0150 = parseURL(down_0150)
+    # dataE_0150 = formatData("E", parsedE_0150, '0150')
+    # dataE_0150 = formatData("S", parsedS_0150, '0150')
+    # parsedE_0600 = parseURL(up_0600)
+    # parsedS_0600 = parseURL(down_0600)
+    # dataE_0600= formatData("E", parsedE_0600, '0600')
+    # dataE_0600= formatData("S", parsedS_0600, '0600')
+
+    csv_E = pd.read_csv('1000_E.csv')
+    csv_S = pd.read_csv('1000_S.csv')
+    currentHr = int(time.strftime("%H")) + 12
+
+    pastFutureData_E = csv_E.values[0:, currentHr - 3:currentHr + 3]
+    pastFutureData_S = csv_S.values[0:, currentHr - 3:currentHr + 3]
+    print(currentHr)
+    tempData['E'] = pastFutureData_E.tolist()
+    tempData['S'] = pastFutureData_S.tolist()
+    # print(tempData)
+    combineData(parsedE_1000, parsedS_1000, '1000')
+    print(tempData)
+
+
+def formatData(direction, sectionList, name):
+    data = {}
+    data['road'] = name
+    data['direction'] = direction
+    data['data'] = sectionList['data']
+    data['name'] = sectionList['name']
+
+    # savedict = open('output1.txt', 'w')
+    # savedict.write(str(data))
+    # savedict.close()
+    return data
+
+
+def combineData(sectionList, name):
+    global tempData
+    tempData['road'] = name
+    temp = insertCurrentData(tempData['data'], sectionList['data'])
+    tempData['data'] = temp
+    tempData['name'] = sectionList['name']
+    tempData['direction'] = sectionList['direction']
+
+
+def insertCurrentData(sectionList, currentList):
+    sectionListLen = len(sectionList)
+    i = 0
+    for rowData in sectionList:
+        rowData.insert(0, int(currentList[i]))
+        i += 1
+
+    return sectionList
+
 
 if __name__ == '__main__':
     print('main')
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '8080')))
-else :
+else:
     print('fucking sub')
